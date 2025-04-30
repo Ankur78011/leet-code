b5465b20ae1348a353e7a731c3be811905175991
Ankur <rohit@ROHITs-MacBook-Air.local>
2025-04-30 18:56:05 +0530

    first-quue

--- merge-strings.js
+++ merge-strings.js
@@ -1,1 +1,29 @@
-console.log('hello world')
+/* 
+
+You are given two strings word1 and word2. Merge the strings by adding letters in alternating order, starting with word1.
+ If a string is longer than the other, append the additional letters onto the end of the merged string.
+
+*/
+let w1="ab"
+let w2="pqrs"
+
+function mergeSrting(w1,w2){
+ let minLen=Math.min(w1.length,w2.length)
+ let ans='';
+ for (let i=0;i<minLen;i++){
+ ans=ans+w1[i]
+ ans=ans+w2[i]
+ }
+ if(w1.length>w2.length){
+    ans=ans+w1.slice(minLen,w1.length)
+ }else{
+    ans=ans+w2.slice(minLen,w2.length)
+ }
+ return ans
+    
+}
+
+
+const res=mergeSrting(w1,w2)
+console.log(res)
+
